a3b6b8a352805b95b57bc0e7578bd19ead2db988
Nick <nmcarducci@gmail.com>
2021-04-25 08:45:59 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          width: "100%"
+        }}
+      >
+        <h1>residualsplit.us</h1>
+        <h1>residual-split</h1>
+        Stimulus doesn’t work for people it is http://residualsplit.us
+        @abbydphillip ...
+        <br />
+        <br />
+        It is also bail-out for usurped debt when credit wasn’t yet counterfeit
+        <br />
+        <br />
+        Taxes-over-$400k for free-rider-mutable labor*assets*resources is
+        monopsony. I’m not jealous of the government, I am concerned for
+        public-health #MinnesotaIsGuilty #ChauvinTrial @chase @MoodysInvSvc
+        #CarFace
+      </div>
+    </div>
+  );
+}
